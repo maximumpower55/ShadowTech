a1a909e25cf6f552f7ffcb404cf16ab7e45c9c84
maximumpower55 <62370320+maximumpower55@users.noreply.github.com>
2020-10-19 00:09:59 -0700

    Changed Name of File

---
+++ src/main/kotlin/ml/visualax/shadowtech/gui/GuiReference.kt
@@ -0,0 +1,11 @@
+package ml.visualax.shadowtech.gui
+
+import net.minecraft.util.Identifier
+
+class GuiReference {
+    companion object {
+        val EnergyBarEmpty: Identifier get() = Identifier("shadowtech", "textures/gui/energy_bar_empty.png")
+
+        val EnergyBarFull: Identifier get() = Identifier("shadowtech", "textures/gui/energy_bar_full.png")
+    }
+}
